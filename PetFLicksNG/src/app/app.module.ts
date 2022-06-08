c8396320d8254b0c0ef31f60150af3ed34a9c895
wilderchris <94183821+wilderchris@users.noreply.github.com>
2022-06-08 13:56:33 -0500

    updating frontend

--- PetFLicksNG/src/app/app.module.ts
+++ PetFLicksNG/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxFileDropModule } from 'ngx-file-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadVideoComponent } from './components/upload-video/upload-video.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadVideoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgxFileDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
